9ea934d8ddf1dd520888408606d5711e8ca394c5
Alex Henrique Dos Santos Ferreira <alexal.14@gmail.com>
2016-10-22 20:29:16 -0200

    Inclusão do display
    Incluindo o display LCD

--- Arduino/alex/alex.ino
+++ Arduino/alex/alex.ino
@@ -1,62 +1,48 @@
-/*CONEXÃO COM O BANCO DE DADOS*/
-#include <Ethernet.h>
-#include <MySQL_Connection.h>
-#include <MySQL_Cursor.h>
+/*Inclusão das bibliotecas*/
+/**Conexão com o banco**/
+  #include <Ethernet.h>
+  #include <MySQL_Connection.h>
+  #include <MySQL_Cursor.h>
+  byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xED, 0xFE };
+  IPAddress server_addr(189,14,200,70);
+  char user[] = "grupo8";
+  char password[] = "1YHXU0EHqt";
+  // Sample query
+  char query[128];
+  char INSERT_DATA[] = "INSERT INTO zadmin_banco.captador_chamada (rf_cod, data_hora, mac_arduino) VALUES (%s,now(),1)";
+  EthernetClient client;
+  MySQL_Connection conn((Client *)&client);
+  MySQL_Cursor cur = MySQL_Cursor(&conn);
+  #include <SoftwareSerial.h>
+  #include <PI_TI.h>
 
-byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-//byte mac_addr[] = { "74", "17", "C1", "4F", "F8", "DD" };
-//74-17-C1-4F-F8-DD
-IPAddress server_addr(189,14,200,70);
-char user[] = "grupo8";
-char password[] = "1YHXU0EHqt";
-// Sample query
-char query[128];
-char INSERT_DATA[] = "INSERT INTO zadmin_banco.captador_chamada (rf_cod, data_hora, mac_arduino) VALUES (%s,now(),1)";
+/**Leitor do RFID**/
+  SoftwareSerial mySerial(7, 20); // O leitor de RFID está na porta D7 do Arduino
+  RFID sensorAcesso(mySerial);
 
-
-
-EthernetClient client;
-MySQL_Connection conn((Client *)&client);
-// Create an instance of the cursor passing in the connection
-MySQL_Cursor cur = MySQL_Cursor(&conn);
-
-
-
-/*LEITOR DO RFID*/
-#include <SoftwareSerial.h>
-#include <PI_TI.h>
-
-// configuração da da software serial para receber numero dos cartões
-
-          //  RX, TX
-SoftwareSerial mySerial(7, 20); // O leitor de RFID está na porta D7 do Arduino
-                // e o Arduino UNO não possui uma porta digital D20 o valor foi colocado 
-                // apenas para poder utilizar a funcão sem desperdiçar uma porta utilizável. 
-RFID sensorAcesso(mySerial);
-
+/**Display**/
+  #include <LiquidCrystal_I2C.h> 
+  LiquidCrystal_I2C lcd(0x3f,2,1,0,4,5,6,7,3, POSITIVE);
 void setup() 
 {
-  // put your setup code here, to run once:
-  pinMode (9, OUTPUT);
-
-  
+  lcd.begin (16,2);
+  lcd.print("Iniciando o Sistema");
+  lcd.clear();
+  delay(1000);
+  pinMode (9, OUTPUT); 
   Serial.begin(9600); // configuração da serial para comunicar com PC.
-  while (!Serial) {
-    ; // aguarda para a porta USB iniciar.
-  }
-
-  // seta a velocidade da porta 
+  while (!Serial) {    ;  }
   mySerial.begin(9600);
   Serial.begin(9600);
   while (!Serial); // wait for serialport to connect
   Ethernet.begin(mac_addr);
-  Serial.println("Connecting...");
+  lcd.print("Conectando...");
   if (conn.connect(server_addr, 3306, user, password)) {
+    lcd.print("Pronto!");
     delay(1000);
-  }
+      }
   else
-    Serial.println("Connection failed.");
-    
+  lcd.print("Falhou.");    
 }
 
 void loop()
@@ -65,18 +51,16 @@ void loop()
 
   if(sensorAcesso.codePronto()) // se ele foi todo recebido
   {
-    Serial.println(sensorAcesso.getCodeDecimal()); //convertemos de hexadecimal, para descimal e imprimimos na tela.
+    lcd.print(sensorAcesso.getCodeDecimal()); //convertemos de hexadecimal, para descimal e imprimimos na tela.
     ;
-    digitalWrite (9, HIGH); 
-    delay (2000);
-    digitalWrite (9,LOW);
     MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
    //  Save
     //dtostrf(1,sensorAcesso.getCodeDecimal(),1,1);
     sprintf(query, INSERT_DATA,sensorAcesso.getCodeDecimal(),mac_addr);
     cur_mem->execute(query);
     delete cur_mem;
-    Serial.println("Dados inseridos.");
+    lcd.clear();
+    lcd.print("Dados inseridos.");
     
     //delay (1000);
     //Serial.println(cur.execute(query));
---
+++ Arduino/display/display.ino/display.ino.ino
@@ -0,0 +1,21 @@
+#include <LiquidCrystal_I2C.h>
+
+// Inicializa o display no endereco 0x27
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
+ 
+void setup()
+{
+ lcd.begin (16,2);
+}
+ 
+void loop()
+{
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(0,5);
+  lcd.print("Arduino e Cia !!");
+  lcd.setCursor(0,6);
+  lcd.print("LCD e modulo I2C");
+  delay(1000);
+  lcd.setBacklight(LOW);
+  delay(1000);
+}
